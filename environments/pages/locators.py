279b3b3983cde42fddd57430846d7e668d27d4a5
“Ksenia <gubanovakseniya@gmail.com>
2020-03-12 17:54:09 +0600

    add methods for login page in base page

--- environments/pages/locators.py
+++ environments/pages/locators.py
@@ -1,13 +1,18 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+##    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-    
-class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form") 
+
 class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
